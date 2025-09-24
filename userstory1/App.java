fefb87c44eeee65891de696f6602982d99ec778c
Yiss <128174682+yxdth3@users.noreply.github.com>
2025-09-24 18:40:28 -0500

    Add files via upload

---
+++ userstory1/App.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.userstory1;
+
+
+import com.mycompany.userstory1.ui.InventarioUI;
+
+
+public class App {
+
+    public static void main(String[] args) { /*el metodo main es el punto de entrada de la aplicación*/
+        new InventarioUI().iniciar();  /*se crea una instancia de la clase la clase InventarioUI y se la pasa el método iniciar.*/
+    }
+}
+
+
+/*el metodo iniciar() muestra el menu de opciones a elegir*/
+
